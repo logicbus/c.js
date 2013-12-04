7b2d1282cc1b6614eece4d339835a16c0e075e3c
Daniel Premo <logicbus@gmail.com>
2013-12-04 17:16:29 -0500

    added c.js file

---
+++ c.js
@@ -0,0 +1,60 @@
+function c(value, label, inDisplay) {
+	var display = inDisplay;
+	var passedDisplay = display;
+	switch (typeof(value)) {
+		case 'object' :
+			if (value == null) {
+				try {
+					console.log('null');
+				} catch(err) {}
+			} else {
+				switch (display) {
+					case 'topexpanded' :
+						try {
+							console.group(label);
+						} catch(err) {}
+						passedDisplay = 'collapsed';
+						break;
+					case 'topcollapsed' :
+						try {
+							console.groupCollapsed(label);
+						} catch(err) {}
+						passedDisplay = 'expanded';
+						break;
+					case 'allcollapsed' :
+					case 'collapsed' :
+						try {
+							console.groupCollapsed(label);
+						} catch(err) {}
+						passedDisplay = 'collapsed';
+						break;
+					case 'allexpanded' :
+					case 'expanded' :
+					default :
+						try {
+							console.group(label);
+						} catch(err) {}
+						passedDisplay = 'expanded';
+						break;
+				}
+				$.each(value, function(index, val) {
+					c(val, index, passedDisplay);
+				});
+				try {
+					console.groupEnd();
+				} catch(err) {}
+			}
+			break;
+		default :
+			if (label == null) {
+				try {
+					console.log(value);
+				} catch(err) {}
+			} else {
+				try {
+					console.log(label + ' : ' + value);
+				} catch(err) {}
+			}
+			break;
+	}
+}
